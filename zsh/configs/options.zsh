9e701ad561307e0d54649449a4100dd1b6deaca0
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2018-10-22 22:54:28 +0300

    Remove auto insert -v

--- zsh/configs/options.zsh
+++ zsh/configs/options.zsh
@@ -9,9 +9,3 @@ setopt globcomplete nocaseglob
 
 # allow [ or ] wherever you want
 unsetopt nomatch
-
-# auto insert -v options
-# for c in cp rm chmod rename mv perl-rename mkdir; do
-#   alias $c="$c -v"
-# done
-
