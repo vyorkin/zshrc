e8d53b8396b5c19a43a3c9f35e4ee8f4413a2a4d
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2020-08-07 19:03:56 +0300

    Update firebase.zsh

--- zconfig/functions/firebase.zsh
+++ zconfig/functions/firebase.zsh
@@ -7,12 +7,18 @@ f() {
           firebase database:get $2 | jid
           ;;
       r)
-          firebase database:remove $2
+          firebase database:remove --confirm $2
           ;;
       u)
-          firebase database:update $2
+          firebase database:update --data $3 --confirm $2 
+          ;;
+      p)
+          firebase database:push --data $3 $2
+          ;;
+      P)
+          firebase database:profile
           ;;
       *)
-          echo "${0} {o - open|g - get|r - remove|u - update}"
+          echo "${0} {o - open|g - get|r - remove|u - update, p - push, P - profile}"
   esac
 }
