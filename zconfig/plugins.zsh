7743f4627d8436c32bf49a788c7ffe37a1f964e6
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2020-12-25 16:10:54 +0300

    Self-manage zplug

--- zconfig/plugins.zsh
+++ zconfig/plugins.zsh
@@ -1,5 +1,6 @@
 . ~/.zplug/init.zsh
 
+zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
