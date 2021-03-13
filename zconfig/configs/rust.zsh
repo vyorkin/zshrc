84a4a5d37ce37db768d1f5a35f23fa39d1fb05d1
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2021-03-13 13:19:02 +0300

    Remove kb function, check for rustc

--- zconfig/configs/rust.zsh
+++ zconfig/configs/rust.zsh
@@ -1,3 +1,5 @@
-export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+if [ -x "$(command -v rustc)" ]; then
+  export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
 
-PATH="$HOME/.cargo/bin:$PATH"
+  PATH="$HOME/.cargo/bin:$PATH"
+fi
--- zconfig/functions/kb.zsh
+++
@@ -1,7 +0,0 @@
-# setup keyboard
-kb() {
-  xset r rate 300 65
-  xcape -e 'Caps_Lock=Escape;Control_L=Escape;Control_R=Escape'
-  xmodmap ~/.Xmodmap
-  xbindkeys
-}
