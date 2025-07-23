a48e6497d8f70e588cb25f564aea8de52839ce75
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2025-07-23 10:57:17 +0300

    Update PATH

--- zconfig/configs/path.zsh
+++ zconfig/configs/path.zsh
@@ -12,11 +12,11 @@ export PATH="$PATH:$HOME/.emacs.d/bin"
 export LESSOPEN="|/opt/homebrew/bin/lesspipe.sh %s"
 
 # rust
-if [ -x "$(command -v rustc)" ]; then
-  export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
-
-  PATH="$HOME/.cargo/bin:$PATH"
-fi
+# if [ -x "$(command -v rustc)" ]; then
+#   export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+#
+#   PATH="$HOME/.cargo/bin:$PATH"
+# fi
 
 export PATH="$PATH:/usr/local/go/bin"
 export PATH="$PATH:/opt/nvim/"
